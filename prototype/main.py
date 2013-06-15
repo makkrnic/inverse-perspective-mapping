d142650f2f4246fc1254e7b6994a9cdb96cd9e33
Mak Krnic <makkrnic@gmail.com>
2013-06-15 18:17:15 +0200

    Add H matrix calculation

--- prototype/main.py
+++ prototype/main.py
@@ -3,6 +3,7 @@
 from PIL import Image
 from pylab import *
 import matplotlib
+from numpy import *
 
 def showPointsSelection(imgPath):
   im = array(Image.open(imgPath))
@@ -11,7 +12,39 @@ def showPointsSelection(imgPath):
   points = ginput(4)
   print (points)
 
+  return points
+
+def calculateHMatrix (originalPoints, transformedPoints):
+  print ("calculating H matrix")
+  
+  orig = np.array([tuple(i) for i in originalPoints]);
+  transformed = np.array([tuple(i) for i in transformedPoints]);
+
+  A = []
+  for i in range (0,4):
+    A.append([0, 0, 0, -orig[i][0], -orig[i][1], -1, transformed[i][1] * orig[i][0], transformed[i][1] * orig[i][1], transformed[i][1]])
+    A.append([orig[i][0], orig[i][1], 1, 0, 0, 0, transformed[i][0] * orig[i][0], transformed[i][0] * orig[i][1], transformed[i][0]])
+
+  A = np.array(A)
+  print (A)
+
+  u, d, vt = linalg.svd(A)
+  print (u)
+  print (d)
+  print (vt)
+
+  h = vt[0]
+  
+  H = np.array([
+    [h[0],h[1],h[2]],
+    [h[3],h[4],h[5]],
+    [h[6],h[7],h[7]]]
+
+  return H
 
 if __name__ == '__main__':
   print ("hello")
-  showPointsSelection('sample.jpg')
+  transformedPoints = [(0,0), (100,0), (100,100), (0,100)]
+  originalPoints = showPointsSelection('sample.jpg')
+
+  hMatrix = calculateHMatrix (originalPoints, transformedPoints)
