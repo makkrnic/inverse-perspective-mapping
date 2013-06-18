3e211eddd1a7a7a4e5f106578499072e9a6621ff
Mak Krnic <makkrnic@gmail.com>
2013-06-19 00:32:41 +0200

    Add support for detecting clicked points

--- c++/inverseMapping.cpp
+++ c++/inverseMapping.cpp
@@ -1,7 +1,63 @@
 #include "inverseMapping.h"
 
-int main (int argc, char *argv[]) {
+using namespace std;
+using namespace cv;
 
+void mouseHandler (int event, int x, int y, int flags, void *param) {
+  switch (event) {
+    case CV_EVENT_LBUTTONDOWN:
+      ((CvPoint*)param)->x = x;
+      ((CvPoint*)param)->y = y;
+      break;
+  }
+}
+
+int main (int argc, char *argv[]) {
+  const char *usage = " <input_file>";
+  char *inputImageName;
+  
+  if (argc < 2) {
+    cerr << "Usage: "<< argv[0] << usage << "\n";
+    return -1;
+  }
+
+  inputImageName = argv[1];
+
+  Mat originalImage = imread (inputImageName, CV_LOAD_IMAGE_COLOR);
+
+  if (!originalImage.data) {
+    cerr << "Could not open image: " << inputImageName << "\n";
+    return -1;
+  }
+
+  // First, we need to display the image to the user
+  namedWindow ("Points selection", CV_WINDOW_NORMAL | CV_WINDOW_KEEPRATIO);
+  // Next, we have to pick up selected points
+  CvPoint selectedPoint;
+  cvSetMouseCallback ("Points selection", mouseHandler, &selectedPoint);
+
+  imshow ("Points selection", originalImage);
+
+  vector<CvPoint> selectedPoints;// = new vector();
+  int pointsClicked;
+  for (pointsClicked = 0; pointsClicked < 4; pointsClicked++) {
+    selectedPoint.x = -1;
+    while (selectedPoint.x == -1) {
+      waitKey(1);
+    }
+    
+    selectedPoints.push_back (selectedPoint);
+
+  }
+
+  cout << "Points: " << "\n";
+  for (int i = 0; i < 4; i++) {
+    CvPoint p = selectedPoints[i];
+    cout << "(" << p.x << ", " << p.y << ")" << "\n";
+  }
+
+  
+  //waitKey (0);
 
   return 0;
 }
--- c++/inverseMapping.h
+++ c++/inverseMapping.h
@@ -1,5 +1,9 @@
-#ifndef _MAPPING_MAIN
-#define _MAPPING_MAIN
+#ifndef _MAPPING_MAIN_H
+#define _MAPPING_MAIN_H
 
+#include <iostream>
+#include <vector>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 #endif
